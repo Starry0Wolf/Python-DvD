2f122128f1a0413e34f3fbe49b2f6103ab28ef86
Starry0Wolf <Starry00Wolf@gmail.com>
2024-03-26 15:54:26 -0400

    Added reapting every 5 seconds

--- label.py
+++ label.py
@@ -16,13 +16,15 @@ image2 = image.subsample(3,3)
 img = Label(root, image=image2)
 img.pack()
 
-
-
 EquX = "less"
 EquY = "more"
 
 
+
+
 def click():
+    global EquX
+    global EquY
     print("X:", root.winfo_x(), "Y:", root.winfo_y(), "  EquX:", EquX, "EquY:", EquY)
     xx = int(root.winfo_x())
     yy = int(root.winfo_y())
@@ -39,36 +41,83 @@ def click():
 
     root.geometry(f'+{xx}+{yy}')
     root.update
+    bounce()
+    root.after(500, click)
 
-Button(text="Get position", command=click).pack()
 
-def click2():
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    print({screen_width}, {screen_height})
+
+
     
 
-Button(text="Screen size", command=click2).pack()
+Button(text="Get position", command=click).pack()
 
 # To top left is both negtive
 # To bottom right is both postive
 # To top right is x postive and y negtive
-# To bottom left is y postive and x negtive
-
-
+# To bottom left is x negtive and y postive
 
 def bounce():
-    if root.winfo_x() == 0:
+    global EquX
+    global EquY
+
+    if root.winfo_x() <= 0:
         print("\033[32mLEFT\033[0m")
+        # Bottom left
+        if EquX == "less" and EquY == "more":
+            EquX = "more"
+            EquY = "more"
+        # Top left
+        elif EquX == "less" and EquY == "less":
+            EquX = "more"
+            EquY = "less"
 
-    if root.winfo_y() == 25:
+
+    if root.winfo_y() <= 25:
         print("\033[32mTOP\033[0m")
+        # Top right
+        if EquX == "more" and EquY == "less":
+            EquX = "less"
+            EquY = "less"
+        # Top left
+        elif EquX == "less" and EquY == "less":
+            EquX = "less"
+            EquY = "more"
+        
 
-    if root.winfo_x() == 1648:
+# To top left is both negtive
+# To bottom right is both postive
+# To top right is x postive and y negtive
+# To bottom left is x negtive and y postive
+
+
+    if root.winfo_x() >= 1648:
         print("\033[32mRIGHT\033[0m")
+        # Top right
+        if EquX == "more" and EquY == "less":
+            EquX = "less"
+            EquY = "less"
+        # Bottom right
+        elif EquX == "more" and EquY == "more":
+            EquX = "less"
+            EquY = "more"
 
-    if root.winfo_y() == 772:
+
+    if root.winfo_y() >= 792:
         print("\033[32mBOTTOM\033[0m")
+        # Bottom right
+        if EquX == "more" and EquY == "more":
+            EquX = "more"
+            EquY = "less"
+        # Bottom left
+        elif EquX == "less" and EquY == "more":
+            EquX = "less"
+            EquY = "less"
+
+
+
+
+
+
 
 Button(text="Bounce?", command=bounce).pack()
 
